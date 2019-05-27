62e77c955ccccaec2bc03aef9efdd999c99df17b
Diana Bejan <diana.bejan@brainloop.com>
2019-05-27 15:27:12 +0300

    update dependencies

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+cd auth-service
+go run cmd/auth-service/main.go &
+cd ../compression-service
+go run cmd/compression-service/main.go &
+cd ../crypto-service
+go run cmd/crypto-service/main.go &
+cd ../file-service
+go run cmd/file-service/main.go &
+cd ../user-service
+go run cmd/user-service/main.go &
+cd ../watermarking-service
+go run cmd/watermarking-service/main.go &
