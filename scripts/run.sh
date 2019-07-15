74967dbb551fe1d7bced411c6a8195632d6f0230
Diana Bejan <diana.bejan.extern@brainloop.com>
2019-07-15 23:21:41 +0300

    add stuff

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-mongod &
+docker run --rm -it -p 27017:27017 mongo   &
 cd auth-service
 go run cmd/auth-service/main.go &
 cd ../compression-service
@@ -6,11 +6,12 @@ go run cmd/compression-service/main.go &
 cd ../crypto-service
 go run cmd/crypto-service/main.go &
 cd ../file-service
-go run cmd/file-service/main.go &
+go run cmd/file-service/main.go cmd/file-service/handlers.go &
 cd ../user-service
-go run cmd/user-service/main.go  cmd/user-service/handlers.go&
+go run cmd/user-service/main.go  cmd/user-service/handlers.go &
 cd ../watermarking-service
 go run cmd/watermarking-service/main.go &
 cd pkg/watermarking/server
 source bits_vm
-python server.py
+python server.py &
+ocker run --rm -it -p 8081:8081 42740d69f4cf &
